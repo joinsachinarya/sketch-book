0408d71d837e1b6a25b39c27aac620f18e276040
Sachin Arya <sachinkarya333@gmail.com>
2024-09-23 04:39:13 +0530

    refactor

---
+++ src/Test.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Test = ({ text = "Hello" }) => {
+  return (
+    <div>{text}</div>
+  )
+}
+
+export default Test
--- src/super-emoji.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const SuperEmoji = ({ emoji = "👋" }) => {
-  return (
-    <span>{emoji}</span>
-  )
-}
-
-export default SuperEmoji
